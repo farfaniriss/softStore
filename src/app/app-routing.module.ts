f248e9cc51f1ad44c0f759d0da19807cbd1b28b6
farfaniriss <iris.farfan.c@gmail.com>
2018-08-17 16:40:01 -0500

    homepage added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { StoreLocatorComponent } from './store-locator/store-locator.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import { CollectionComponent } from './collection/collection.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+  {
+    path: "products/:id",
+    component: ProductDetailComponent
+  },
+  {
+    path: "faqs",
+    component: FaqsComponent
+  },
+  {
+    path: "store-locator",
+    component: StoreLocatorComponent
+  },
+  {
+    path: "collection/:id",
+    component: CollectionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProductsComponent } from './products/products.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { StoreLocatorComponent } from './store-locator/store-locator.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import { CollectionComponent } from './collection/collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    FaqsComponent,
+    StoreLocatorComponent,
+    ProductDetailComponent,
+    HomeComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
